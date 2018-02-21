fc35ed819e67a4d24a872fb626665db92b988cdd
Fabrício Silva <fabriciofmsilva@gmail.com>
2018-02-21 11:33:34 -0300

    Add webpack config file

---
+++ webpack/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // path relative to this file
+    entry: './src/scripts/app.js',
+    output: {
+        // path relative to this file
+        filename: './dist/app.bundle.js'
+    }
+};
